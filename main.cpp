c5722d16d6b6b0739b719f0b3192b104e8331cd9
Minebom55 <alfred.brunberg@icloud.com>
2026-02-18 22:45:14 +0100

    Working
    Basic working pi calculating funtionality

--- main.cpp
+++ main.cpp
@@ -1,20 +1,60 @@
 #include <random>
 #include <iostream>
+int miss = 0;
+int hit = 0;
+int ggr = 100000000;
+int oggr = ggr;
+
+
+
+
+long double pyt(long double a, long double b) {
+    long double a2 = a*a;
+    long double b2 = b*b;
+
+    return sqrt(a2 + b2);
+}
+
+// random number generator
+std::random_device rd;
+std::mt19937 gen(rd());
+
+std::uniform_real_distribution<long double> dist(-1.0L, 1.0L);
 
 int main() {
-    // random number generator
-    std::random_device rd;
-    std::mt19937 gen(rd());
+    while (ggr > 0) {
 
-    // uniform distribution between -1.0 and 1.0
-    std::uniform_real_distribution<long double> dist(-1.0L, 1.0L);
 
-    // generate values
-    long double xPos = dist(gen);
-    long double yPos = dist(gen);
+        long double xPos = dist(gen);
+        long double yPos = dist(gen);
 
-    std::cout << xPos << "\n";
-    std::cout << yPos << "\n";
+        /*debuging
+        std::cout << xPos << "\n";
+        std::cout << yPos << "\n";
+        */
+
+        long double pyth = pyt(xPos, yPos);
+        //std::cout << pyth << std::endl;
+
+        if (pyth <= 1.0L) {
+            hit++;
+            //std::cout << hit << std::endl;
+        }
+        else {
+            miss++;
+            //std::cout << miss << std::endl;
+        }
+        ggr--;
+        }
+
+
+    long double output_pi = 4.0L * hit / oggr;
+
+    std::cout << "------------------" << std::endl;
+    std::cout << "pi: " << output_pi << std::endl;
+    std::cout << "total points: " << oggr << std::endl;
+    std::cout << "hits: " << hit << std::endl;
+    std::cout << "misses: " << miss << std::endl;
 
     return 0;
 }
